4c4c2fb20d22d0bebe273244b7e5e0b3207f5767
Kamaal ABOOTHALIB <kamaal@kamaal.me>
2025-01-30 13:21:05 +0530

    feat(doc): rollup type

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,48 +1,42 @@
-import { readFileSync } from 'fs';
-import { defineConfig } from 'rollup';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
-import typescript from '@rollup/plugin-typescript';
-import typescriptEngine from 'typescript';
+import { readFileSync } from "fs";
+import { defineConfig } from "rollup";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import typescriptEngine from "typescript";
 
-const packageJson = JSON.parse(readFileSync('./package.json'));
+const packageJson = JSON.parse(readFileSync("./package.json"));
 
-export default defineConfig(
-{
-	input: './src/index.ts',
-	output: [
-		{
-			file: packageJson.module,
-			format: 'es',
-			exports: 'named',
-			sourcemap: false,
-		},
-	],
-	plugins: [
-		resolve(),
-		commonjs(),
-		typescript({
-			tsconfig: './tsconfig.json',
-			typescript: typescriptEngine,
-			sourceMap: false,
-			exclude: [
-				'coverage',
-				'dist',
-				'node_modules/**',
-				'*.cjs',
-				'*.mjs',
-				'**/__tests__',
-				'**/*.test.js',
-				'**/*.test.ts',
-				'setupTests.ts'
-			],
-		}),
-		terser(),
-	],
-},
-{
-	input: 'dist/src/index.d.ts',
-	output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-},
-);
+export default defineConfig({
+  input: "./src/index.ts",
+  output: [
+    {
+      file: packageJson.module,
+      format: "cjs",
+      exports: "auto",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: "./tsconfig.json",
+      typescript: typescriptEngine,
+      sourceMap: false,
+      exclude: [
+        "coverage",
+        "dist",
+        "node_modules/**",
+        "*.cjs",
+        "*.mjs",
+        "**/__tests__",
+        "**/*.test.js",
+        "**/*.test.ts",
+        "setupTests.ts",
+      ],
+    }),
+    terser(),
+  ],
+});
